a2c88b3d6f87673313456401b2547d962e35f1cc
matthcol <matthias.colin@gmail.com>
2026-05-27 08:53:34 +0200

    mardi

---
+++ PooHook/tests/test_utils.py
@@ -0,0 +1,14 @@
+from collections.abc import Iterator
+
+from utils import skip
+
+
+def test_skip_empty():
+    it = skip([], 0)
+    assert isinstance(it, Iterator)
+    assert list(it) == []
+
+def test_skip_over_the_limit():
+    it = skip([1, 2, 3], 10)
+    assert isinstance(it, Iterator)
+    assert list(it) == []
---
+++ PooHook/utils.py
@@ -0,0 +1,27 @@
+from typing import Iterable
+
+# TODO: improve typing
+def skip(iterable: Iterable, n: int):
+    """Return an iterator on the iterable object which has advanced of n steps.
+
+    Parameters:
+    - n : number of elements to skip (must be positive or null)
+    """
+    assert n >= 0, "number of skipped values must be positive or null"
+    it = iter(iterable)
+    try:
+        for _ in range(n):
+            next(it)
+    except StopIteration:
+        pass
+    return it
+
+# tests manuels => tests auto
+if __name__ == '__main__':
+    print(list(skip([1, 2 , 3], 0)))
+    print(list(skip([1, 2 , 3], 1)))
+    print(list(skip([1, 2 , 3], 2)))
+    print(list(skip([1, 2 , 3], 3)))
+    print(list(skip([1, 2 , 3], 4)))
+    # print(list(skip([1, 2 , 3], -1))) # print(list(skip([1, 2 , 3], -1)))
+    # print(list(skip(123, 2))) # Linter error + dynamic error : TypeError: 'int' object is not iterable
